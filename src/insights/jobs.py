7bd40557abbd8655e11f0c35ea0dfeed9e6a94fd
JyojiTenguam <j.y.oji@hotmail.com>
2024-09-26 04:18:03 -0300

    Resolvendo requisito 02 de Job Insights

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,12 +1,16 @@
 from typing import List, Dict
+import csv
 
 
 class ProcessJobs:
     def __init__(self) -> None:
         self.jobs_list = list()
 
-    def read(self) -> List[Dict]:
-        pass
+    def read(self, file_path: str) -> List[Dict]:
+        with open(file_path, 'r') as file:
+            reader = csv.DictReader(file)
+            self.jobs_list = list(reader)
+        return self.jobs_list
 
     def get_unique_job_types(self) -> List[str]:
         pass
--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -3,5 +3,4 @@ from src.pre_built.counter import count_ocurrences
 
 def test_counter():
     path = 'data/jobs.csv'
-
     assert count_ocurrences(path, 'Ruby') == 83
